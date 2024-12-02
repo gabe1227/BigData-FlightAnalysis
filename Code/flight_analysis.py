7b1c5d31539456b4f36f0a89c54a8ef928957a08
graysonmorris31 <morris31@colostate.edu>
2024-12-01 17:31:04 -0700

    file path changes

--- Code/flight_analysis.py
+++ Code/flight_analysis.py
@@ -12,8 +12,8 @@ import pandas as pd
 # Start a spark session for the cluster
 spark = SparkSession.builder.appName("FlightAnalysis").getOrCreate()
 
-df_2019 = spark.read.option("header", "true").csv("~/BigData-FlightAnalysis/Data/2019.csv")
-df_2023 = spark.read.option("header", "true").csv("~/BigData-FlightAnalysis/Data/2023.csv")
+df_2019 = spark.read.option("header", "true").csv("/Data/2019.csv")
+df_2023 = spark.read.option("header", "true").csv("/Data/2023.csv")
 
 # Convert to Pandas DataFrame for better display
 df_2019_pd = df_2019.limit(5).toPandas()
@@ -241,6 +241,6 @@ kmeans_predictions_23_with_str = kmeans_predictions_23.withColumn("features_str"
 future_predictions_with_str = future_predictions_filtered.withColumn("features_str", vector_to_string_udf("features")).drop("features")
 
 # Save all results to Google Drive
-kmeans_predictions_19_with_str.write.format("csv").option("header", "true").mode("overwrite").save("~/BigData-FlightAnalysis/Data/kmeans_predictions_19.csv")
-kmeans_predictions_23_with_str.write.format("csv").option("header", "true").mode("overwrite").save("~/BigData-FlightAnalysis/Data/kmeans_predictions_23.csv")
-future_predictions_with_str.write.format("csv").option("header", "true").mode("overwrite").save("~/BigData-FlightAnalysis/Data/future_predictions.csv")
+kmeans_predictions_19_with_str.write.format("csv").option("header", "true").mode("overwrite").save("/Data/kmeans_predictions_19.csv")
+kmeans_predictions_23_with_str.write.format("csv").option("header", "true").mode("overwrite").save("/Data/kmeans_predictions_23.csv")
+future_predictions_with_str.write.format("csv").option("header", "true").mode("overwrite").save("/Data/future_predictions.csv")
